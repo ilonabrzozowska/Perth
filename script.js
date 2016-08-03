056da00f696cd205ec48c5945c53818effb8d2f1
Ilona Brzozowska <C:\The Bat!\Mail>
2016-08-03 16:05:38 +0200

    Changes in header to be more pixel perfect, changes in opening navigation script

--- script.js
+++ script.js
@@ -1,24 +1,15 @@
 $(document).ready(function() {
     $('.hamburger').click(function(){
 		$(this).toggleClass('open');
-        if($('.logo').css('opacity') != 0.1)
+        if($(this).hasClass('open'))
         {
-            $('.logo').css('opacity', 0.1);
-            $('.banner').css('opacity', 0.1);
-            $('.iPhone5-header-image').css('opacity', 0.1);
-            $('.page-header').css('background-color', '#e57f85');
             $('.navigation-menu').css('display', 'block');
-            $('.banner-text').css('padding-top', 0);
+            $('.page-header, .logo, .banner, .iPhone5-header-image').addClass('opac');
         }
         else
         {
-            $('.logo').css('opacity', 1);
-            $('.banner').css('opacity', 1);
-            $('.iPhone5-header-image').css('opacity', 1);
-            $('.page-header').css('background-color', '#61c0e7');
             $('.navigation-menu').css('display', 'none');
-            $('.banner-text').css('padding-top', 213);
-
+            $('.page-header, .logo, .banner, .iPhone5-header-image').removeClass('opac');
         }
 	});
 })
