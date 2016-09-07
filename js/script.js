d3c271937bddcf1ca9d82380b79fb3e86b3920c5
Ilona Brzozowska <C:\The Bat!\Mail>
2016-09-07 09:09:24 +0200

    Changing js file - get started with JSLint. Trying to repair fonts and icons.

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
-$(document).ready(function() {
-    $('.hamburger').click(function(){
-		$(this).toggleClass('open');
-        if($(this).hasClass('open'))
-        {
+/*jslint browser: true*/
+/*global  $*/
+$(document).ready(function () {
+    'use strict';
+    $('.hamburger').click(function () {
+        $(this).toggleClass('open');
+        if ($(this).hasClass('open')) {
             $('.navigation-menu').addClass('nav-open');
             $('.page-header, .logo, .banner, .iPhone5-header-image').addClass('opac');
-        }
-        else
-        {
+        } else {
             $('.navigation-menu').removeClass('nav-open');
             $('.page-header, .logo, .banner, .iPhone5-header-image').removeClass('opac');
         }
-	});
-})
+    });
+});
